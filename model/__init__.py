28dd59715684d829f355e4fe304a2169ce787ce5
kindle zhang <kindle0118@163.com>
2025-11-05 23:25:50 -0500

    update

--- model/__init__.py
+++ model/__init__.py
@@ -1,22 +1,12 @@
-# import sys
-# sys.path.append(r"D:\Codes\PyTorch_nn")
-# from model.resnet import resnet18, resnet50
-
-# model_dict = {
-#     'resnet18': resnet18,
-#     'resnet50': resnet50,
-# }
-
-# def create_model(model_name, num_classes):   
-#     return model_dict[model_name](num_classes = num_classes)
 
 import sys
 sys.path.append(r"/Users/kindle\Desktop/file/project/pytorch_nn")
-from model.cnn import simplecnn
+from model.resnet import resnet18, resnet50
 from model.vit_2 import vit_base_patch16_224
 
 model_dict = {
-    "cnn" : simplecnn,
+    'resnet18': resnet18,
+    'resnet50': resnet50,
     'vit' : vit_base_patch16_224
 }
 
