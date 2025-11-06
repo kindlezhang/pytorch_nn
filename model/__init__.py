bcd4d821c63eb3ba592accfb3b6377fb8a95abf5
kindle zhang <kindle0118@163.com>
2025-11-05 23:20:03 -0500

    update

--- model/__init__.py
+++ model/__init__.py
@@ -11,11 +11,13 @@
 #     return model_dict[model_name](num_classes = num_classes)
 
 import sys
-sys.path.append(r"\Users\kindle\Desktop\file\project\pytorch_nn")
+sys.path.append(r"/Users/kindle\Desktop/file/project/pytorch_nn")
+from model.cnn import simplecnn
 from model.vit_2 import vit_base_patch16_224
 
 model_dict = {
-    'vit' : vit_base_patch16_224,
+    "cnn" : simplecnn,
+    'vit' : vit_base_patch16_224
 }
 
 def create_model(model_name, num_classes):   
